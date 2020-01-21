abdd21366a78067b85fd20dc725224da3379c510
Abel Kalum <chumbabel@gmail.com>
2020-01-21 13:40:57 +0000

    Automatically backed up by Learn

--- greeting.rb
+++ greeting.rb
@@ -1,13 +1,9 @@
-def say_hello_world_ten_times
-  phrase = "Hello World!"
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
+def greeting
+  puts "Hello World"
 end
+
+greeting
+greeting
+greeting
+greeting
+greeting
